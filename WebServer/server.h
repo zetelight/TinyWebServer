7e91aebb1915d66f5c4d502db015d9b9d7ba9259
Mantai Lin <mantail@DESKTOP-C1M12PA.localdomain>
2018-03-04 18:15:20 -0800

    header (use textbook source)

--- WebServer/server.h
+++ WebServer/server.h
@@ -2,12 +2,14 @@
 * Author: Adam Chen
 * Description: include head files releated about networking programming
 */
-#ifndef SERVER_HPP_
-#define SERVER_HPP_
+#ifndef SERVER_H_
+#define SERVER_H_
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,5 +26,35 @@
 #include <fcntl.h>
 
 int socket(int domain,int type,int protocol); 
+
+/* Persistent state for the robust I/O (Rio) package */
+/* $begin rio_t */
+#define RIO_BUFSIZE 8192
+typedef struct {
+    int rio_fd;                /* Descriptor for this internal buf */
+    int rio_cnt;               /* Unread bytes in internal buf */
+    char *rio_bufptr;          /* Next unread byte in internal buf */
+    char rio_buf[RIO_BUFSIZE]; /* Internal buffer */
+} rio_t;
+/* $end rio_t */
+
+#define	MAXLINE	 8192  /* Max text line length */
+#define MAXBUF   8192  /* Max I/O buffer size */
+
+void unix_error(char *msg);
+
+/* Rio (Robust I/O) package */
+ssize_t rio_readn(int fd, void *usrbuf, size_t n);
+ssize_t rio_writen(int fd, void *usrbuf, size_t n);
+void rio_readinitb(rio_t *rp, int fd); 
+ssize_t	rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t	rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+
+/* Wrappers for Rio package */
+ssize_t Rio_readn(int fd, void *usrbuf, size_t n);
+void Rio_writen(int fd, void *usrbuf, size_t n);
+void Rio_readinitb(rio_t *rp, int fd); 
+ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
 
 #endif /* SERVER_H_ */
