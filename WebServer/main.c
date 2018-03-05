7c569ac72c6fd19321a3b000b04928448f020f42
Mantai Lin <mantail@DESKTOP-C1M12PA.localdomain>
2018-03-04 18:14:32 -0800

    main c file

--- WebServer/main.c
+++ WebServer/main.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
+#include "server.h"
 
 #define PORT 5000
 #define MAX_QUEUE 5 // the size of listen queue
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
     int port;                       /* port number               */
     struct sockaddr_in client_addr; /* client address            */
     struct sockaddr_in server_addr; /* server address            */
-    char buf[BUFF_SIZE];            /* Buffer to store html info */
+    //char buf[BUFF_SIZE];            /* Buffer to store html info */
     socklen_t client_addr_len = sizeof(client_addr);
 
     /* Check arguments */
@@ -77,15 +78,54 @@ int main(int argc, char *argv[])
 
         /* Send response to client*/
         // hardcode the HTTP request
-        sprintf(buf, "HTTP/1.0 200 OK\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-        send(client_fd, buf, strlen(buf), 0);
-        sprintf(buf, "Content-Type: text/html\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-        strcpy(buf, "\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-        sprintf(buf, "YES. I AM NOT JOKING! \r\n");
-        send(client_fd, buf, strlen(buf), 0);
+        rio_t rio;
+        struct stat sbuf;
+        char buf[MAXLINE], command[MAXLINE], uri[MAXLINE], version[MAXLINE], request[MAXLINE], write_in[MAXBUF];
+        char filename[MAXLINE];
+        Rio_readinitb(&rio,client_fd);
+        Rio_readlineb(&rio,buf,MAXLINE);
+        sscanf(buf, "%s %s %s", command, uri, version);
+        if (strcasecmp(command,"GET")){
+            printf("Not implemented this command\n");
+            exit(EXIT_FAILURE);
+        }
+        Rio_readlineb(&rio, request, MAXLINE);
+        while(strcmp(request,"\r\n")){
+            Rio_readlineb(&rio, request, MAXLINE);
+            printf("%s", request);
+        }
+
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        if (uri[strlen(uri)-1] == '/'){
+            strcat(filename, "home.html");
+        }
+
+        if (stat(filename,&sbuf) < 0){
+            printf("File not found\n");
+            close(client_fd);
+            continue;
+        }
+
+        if (!(S_ISREG(sbuf.st_mode))|| !(S_IRUSR & sbuf.st_mode)){
+            printf("no permission to read this file\n");
+            close(client_fd);
+            continue;
+        }
+        char filetype_default[MAXLINE];
+        strcpy(filetype_default, "text/html");
+        sprintf(write_in, "HTTP/1.0 200 OK\r\n");
+        sprintf(write_in, "%sServer: Tiny Web Server\r\n", write_in);
+        sprintf(write_in, "%sContent-length: %ld\r\n", write_in, sbuf.st_size);
+        sprintf(write_in, "%sContent-type: %s\r\n\r\n", write_in, filetype_default);
+        Rio_writen(client_fd, write_in, strlen(write_in));
+
+        int source = open(filename, O_RDONLY, 0);
+        char *scptr = mmap(0, sbuf.st_size, PROT_READ, MAP_PRIVATE, source, 0);
+        close(source);
+        Rio_writen(client_fd, scptr, sbuf.st_size);
+        munmap(scptr, sbuf.st_size);
+
 
         /* closen client socket */
         close(client_fd);
