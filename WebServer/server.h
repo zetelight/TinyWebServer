8e6e7464b842d78fcee21405a312d5ec5edbb314
Mantai Lin <mantail@uoregon.edu>
2018-03-06 16:17:10 -0800

    functional header

--- WebServer/server.h
+++ WebServer/server.h
@@ -5,27 +5,31 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <strings.h>
 #include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 #include <errno.h>
-#include <malloc.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <stdarg.h>
-#include <fcntl.h>
-#include <fcntl.h>
 
 int socket(int domain,int type,int protocol); 
+
+extern char **environ; /* Defined by libc */
 
 /* Persistent state for the robust I/O (Rio) package */
 /* $begin rio_t */
