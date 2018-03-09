6417b0d2d9a5f7ea026b548ae89e6f34980faaaf
Adam Chen <a461112804@gmail.com>
2018-03-09 12:04:37 -0800

    thread draft 1

--- WebServer/main.c
+++ WebServer/main.c
@@ -9,7 +9,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/stat.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <pthread.h>
 
 #define PORT 5000
@@ -19,6 +19,31 @@
 #define FALSE 0
 #define ADDRESS "test/trivia.html"
 
+void *connectTread(void *id)
+{
+    int client_fd = *(int *)&id;
+    char buf[BUFF_SIZE]; /* Buffer to store html info */
+    pthread_detach(pthread_self());
+    while (1)
+    {
+        /* Send response to client*/
+        sprintf(buf, "HTTP/1.0 200 OK\r\n");
+        send(client_fd, buf, strlen(buf), 0);
+        sprintf(buf, "Content-Type: text/html\r\n");
+        send(client_fd, buf, strlen(buf), 0);
+        strcpy(buf, "\r\n");
+        send(client_fd, buf, strlen(buf), 0);
+
+        /* Send the HTTP content*/
+        sprintf(buf, "YES. I AM NOT JOKING! \r\n");
+        send(client_fd, buf, strlen(buf), 0);
+    }
+    /* closen client socket */
+    close(client_fd);
+    pthread_exit(NULL);
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     int server_fd;                  /* server file descriptor    */
@@ -26,7 +51,6 @@ int main(int argc, char *argv[])
     int port;                       /* port number               */
     struct sockaddr_in client_addr; /* client address            */
     struct sockaddr_in server_addr; /* server address            */
-    char buf[BUFF_SIZE];            /* Buffer to store html info */
     socklen_t client_addr_len = sizeof(client_addr);
 
     /* Check arguments */
@@ -69,6 +93,7 @@ int main(int argc, char *argv[])
         printf("Listening socket failed\n");
         exit(1);
     }
+    pthread_t thread_id;
 
     /* Start to listen request forever */
     while (TRUE)
@@ -80,20 +105,17 @@ int main(int argc, char *argv[])
             printf("Accpeting a request failed\n");
             exit(1);
         }
-        /* Send response to client*/ 
-        sprintf(buf, "HTTP/1.0 200 OK\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-        sprintf(buf, "Content-Type: text/html\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-        strcpy(buf, "\r\n");
-        send(client_fd, buf, strlen(buf), 0);
-
-        /* Send the HTTP content*/
-        sprintf(buf, "YES. I AM NOT JOKING! \r\n");
-        send(client_fd, buf, strlen(buf), 0);
-
-        /* closen client socket */
-        close(client_fd);
+        // Create a new thread
+        int t_id;
+        t_id = pthread_create(&thread_id, NULL, connectTread, (void *)&client_fd);
+        if (t_id < 0)
+        {
+            perror("Creating threads failed");
+        }
+        else
+        {
+            printf("Thread %d is running \n", t_id);
+        }
     }
     /* closen server socket */
     close(server_fd);
